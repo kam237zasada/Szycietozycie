3fc167d468a9903ab8587cc6c74f617c11ac302e
Kamil_Zasada <kam237zasada@wp.pl>
2020-03-09 07:29:16 +0100

    Updates

---
+++ precache-manifest.13df9735da64f12f4a24fba3ad4fca1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715b17fa358fa07f37884ebfd6782b83",
+    "url": "/Szycietozycie/index.html"
+  },
+  {
+    "revision": "e3e0acaea41eb95f72ee",
+    "url": "/Szycietozycie/static/css/main.a25ed23a.chunk.css"
+  },
+  {
+    "revision": "495707f3a9bddb8a3960",
+    "url": "/Szycietozycie/static/js/2.5208a9e6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Szycietozycie/static/js/2.5208a9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e0acaea41eb95f72ee",
+    "url": "/Szycietozycie/static/js/main.400367f1.chunk.js"
+  },
+  {
+    "revision": "a310349c4ba120206996",
+    "url": "/Szycietozycie/static/js/runtime-main.cc8be783.js"
+  },
+  {
+    "revision": "b2ed24e338040ed9b79a1db0e39dcdc9",
+    "url": "/Szycietozycie/static/media/spodnie.b2ed24e3.jpg"
+  }
+]);
